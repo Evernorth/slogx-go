15c1097203482a2cd24f454f659136e9d0c3cb67
Shellee Stewart <10214065+shelleeboo@users.noreply.github.com>
2024-10-14 14:04:18 -0400

    Add example HTTP server with context-aware logging
    - Implement an HTTP server that logs requests with context support.
    - Use `slogx` to build a logger with JSON format and context handler.
    - Manage log levels using environment variables with `LevelManager`.

--- internal/examples/example.go
+++ internal/examples/example.go
@@ -1,1 +1,64 @@
 package main
+
+import (
+	"context"
+	"github.com/Evernorth/slogx-go/slogx"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+var (
+	// This gets us a slog.Logger with context support that logs in JSON format to stdout.
+	logger, levelVar = slogx.NewLoggerBuilder().
+		WithWriter(os.Stdout).
+		WithFormat(slogx.FormatJSON).
+		WithLevel(slog.LevelInfo).
+		WithContextHandler().
+		Build()
+)
+
+func handler(response http.ResponseWriter, request *http.Request) {
+
+	// Get the ctx from the request
+	ctx := request.Context()
+
+	// Extract the id from the query parameters
+	id := request.URL.Query().Get("id")
+
+	// Log the request
+	logger.InfoContext(ctx, "Received request", slog.String("id", id))
+
+	// Write the response
+	response.WriteHeader(http.StatusOK)
+	_, err := response.Write([]byte("Id: " + id))
+	if err != nil {
+		return
+	}
+
+}
+func init() {
+	// Set the default level manager
+	// Enroll the levelVar to be managed from an environment variable.
+	err := slogx.GetLevelManager().ManageLevelFromEnv(levelVar, "APP_LOG_LEVEL")
+	if err != nil {
+		panic(err)
+	}
+
+	// Trigger the LevelManager to update the levels from the environment.
+	// This should be done again if the environment is updated, so that the logger level
+	// can be updated without restarting the application.
+	slogx.GetLevelManager().UpdateLevels()
+
+	slog.InfoContext(context.Background(), "Logger initialized", slog.String("level", levelVar.Level().String()))
+}
+
+func main() {
+
+	http.HandleFunc("/", handler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
