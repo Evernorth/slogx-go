cde4c8696e8bec22ed70a76819cda89136e2fadf
Shellee Stewart <10214065+shelleeboo@users.noreply.github.com>
2024-10-16 23:56:57 -0400

    Pulled from initial-update branch: - CHANGELOG.md - CODE_OF_CONDUCT.md - LICENSE - NOTICE - level-manager.go

--- slogx/level-manager.go
+++ slogx/level-manager.go
@@ -6,23 +6,31 @@ import (
 	"sync"
 )
 
+// LevelManager is an interface for managing slog.LevelVar objects from environment variables.  Call ManageLevelFromEnv to
+// associate a slog.LevelVar with an environment variable key.  Call UpdateLevels to update the levels of all enrolled
+// slog.LevelVar objects from their environment variables.
 type LevelManager interface {
 	ManageLevelFromEnv(levelVar *slog.LevelVar, key string) error
 	UpdateLevels()
 }
 
+// defaultLevelManager is the default implementation of LevelManager.
 type defaultLevelManager struct {
 	levelVarMap *sync.Map
 }
 
+// defaultLevelManagerInstance is the singleton instance of defaultLevelManager.
 var defaultLevelManagerInstance = &defaultLevelManager{
 	levelVarMap: new(sync.Map),
 }
 
+// GetLevelManager returns the singleton instance of LevelManager.
 func GetLevelManager() LevelManager {
 	return defaultLevelManagerInstance
 }
 
+// ManageLevelFromEnv associates a slog.LevelVar with an environment variable key.  The level of the slog.LevelVar will be
+// updated when UpdateLevels is called.
 func (lm *defaultLevelManager) ManageLevelFromEnv(levelVar *slog.LevelVar, key string) error {
 	if levelVar == nil {
 		return eris.New("levelVar is required")
@@ -34,6 +42,7 @@ func (lm *defaultLevelManager) ManageLevelFromEnv(levelVar *slog.LevelVar, key s
 	return nil
 }
 
+// UpdateLevels updates the levels of all enrolled slog.LevelVar objects from their environment variables.
 func (lm *defaultLevelManager) UpdateLevels() {
 	lm.levelVarMap.Range(func(key, value interface{}) bool {
 		var levelVar *slog.LevelVar
